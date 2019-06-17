21d0f622b2442fc12e41900e04e96d77a5890fef
wolfpro231 <48035876+wolfpro231@users.noreply.github.com>
2019-06-17 03:17:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("")
+let channel =     client.channels.get("582216605270802446")
 setInterval(function() {
-channel.send(``);
+channel.send(`King wolf god  King wolf god King wolf god`);
 }, 30)
 })
 
